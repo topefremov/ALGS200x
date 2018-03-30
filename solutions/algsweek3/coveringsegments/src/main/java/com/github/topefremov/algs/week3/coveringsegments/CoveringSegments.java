611a6e18fe68076dd73fb8c204d9127e7c743f4a
Aleksandr Efremov <topefremov@yandex.ru>
2018-03-30 20:30:49 +0300

    add CoveringSegments

--- solutions/algsweek3/coveringsegments/src/main/java/com/github/topefremov/algs/week3/coveringsegments/CoveringSegments.java
+++ solutions/algsweek3/coveringsegments/src/main/java/com/github/topefremov/algs/week3/coveringsegments/CoveringSegments.java
@@ -4,9 +4,39 @@ import java.util.*;
 
 public class CoveringSegments {
 
+    static Segment getMinEndPointSegmentAndSwap(Segment[] segments, int startWithIndex, int swapIndex) {
+        int minEndPointSegmentIndx = startWithIndex;
+        for (int i = startWithIndex; i < segments.length; i++ )
+            if (segments[i].end < segments[minEndPointSegmentIndx].end)
+                minEndPointSegmentIndx = i;
+        Segment minEndPointSegment = segments[minEndPointSegmentIndx];
+        segments[minEndPointSegmentIndx] = segments[swapIndex];
+        segments[swapIndex] = minEndPointSegment;
+        return minEndPointSegment;
+    }
+
+    private static int[] convertArrayInt(Collection<Integer> coll) {
+        Iterator<Integer> iterator = coll.iterator();
+        int[] arr = new int[coll.size()];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = iterator.next();
+        return arr;
+    }
+
     static int[] optimalPoints(Segment[] segments) {
-        Set<Segment> segmentsSet = new HashSet<>(Arrays.asList(segments));
-        return null;
+        Segment[] segmentsCopy = segments.clone();
+        Segment minEndPointSegment = getMinEndPointSegmentAndSwap(segmentsCopy,0,0);
+        List<Integer> points = new ArrayList<>();
+        int currentPoint = minEndPointSegment.end;
+        for(int i = 1; i < segments.length; i++) {
+            Segment candidate = getMinEndPointSegmentAndSwap(segmentsCopy, i, i);
+            if (candidate.start > currentPoint) {
+                points.add(currentPoint);
+                currentPoint = candidate.end;
+            }
+        }
+        points.add(currentPoint);
+        return convertArrayInt(points);
     }
 
     public static class Segment {
@@ -22,20 +52,6 @@ public class CoveringSegments {
         }
 
         @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Segment segment = (Segment) o;
-            return start == segment.start &&
-                    end == segment.end;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(start, end);
-        }
-
-        @Override
         public String toString() {
             return "Segment{" +
                     "start=" + start +
@@ -44,21 +60,21 @@ public class CoveringSegments {
         }
     }
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        int n = scanner.nextInt();
-//        Segment[] segments = new Segment[n];
-//        for (int i = 0; i < n; i++) {
-//            int start, end;
-//            start = scanner.nextInt();
-//            end = scanner.nextInt();
-//            segments[i] = new Segment(start, end);
-//        }
-//        int[] points = optimalPoints(segments);
-//        System.out.println(points.length);
-//        for (int point : points) {
-//            System.out.print(point + " ");
-//        }
-        printRandomDataset();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Segment[] segments = new Segment[n];
+        for (int i = 0; i < n; i++) {
+            int start, end;
+            start = scanner.nextInt();
+            end = scanner.nextInt();
+            segments[i] = new Segment(start, end);
+        }
+        int[] points = optimalPoints(segments);
+        System.out.println(points.length);
+        for (int point : points) {
+            System.out.print(point + " ");
+        }
+//        printRandomDataset();
     }
 
     private static void printRandomDataset() {
--- solutions/algsweek3/coveringsegments/src/test/java/com/github/topefremov/algs/week3/coveringsegments/CoveringSegmentsTest.java
+++ solutions/algsweek3/coveringsegments/src/test/java/com/github/topefremov/algs/week3/coveringsegments/CoveringSegmentsTest.java
@@ -13,6 +13,19 @@ import static com.github.topefremov.algs.week3.coveringsegments.CoveringSegments
 public class CoveringSegmentsTest {
 
     @Test
+    public void getMinEndPointSegmentAndSwapTest() {
+        Segment s1 = new Segment(4, 7);
+        Segment s2 = new Segment(1, 3);
+        Segment s3 = new Segment(2, 5);
+        Segment s4 = new Segment(5, 6);
+        Segment[] segments = new Segment[] {s1,s2,s3,s4};
+        Segment[] expectedSegments = new Segment[] {s2,s1,s3,s4};
+        Segment minEndPointSegment = getMinEndPointSegmentAndSwap(segments, 0,0);
+        assertEquals(s2, minEndPointSegment);
+        assertArrayEquals(expectedSegments, segments);
+    }
+
+    @Test
     public void optimalPointsTest1() {
         Segment segment1 = new Segment(1, 3);
         Segment segment2 = new Segment(2, 5);
@@ -31,7 +44,7 @@ public class CoveringSegmentsTest {
         Segment segment4 = new Segment(5, 6);
         int[] expectedPoints = new int[] {3, 6};
         int[] optimalPoints = optimalPoints(new Segment[] {segment1, segment2, segment3, segment4});
-        assertEquals(optimalPoints.length, 2);
+        assertEquals(2, optimalPoints.length);
         assertArrayEquals(expectedPoints, optimalPoints);
     }
     @Test
