e2787e8aaf9bbb22805557e0c25908cad5874222
Aleksandr Efremov <topefremov@yandex.ru>
2018-07-23 22:09:00 +0300

    Add PrimitiveCalculator

--- solutions/algsweek5/primcalc/src/main/java/com/github/topefremov/algsweek5/primcalc/PrimitiveCalculator.java
+++ solutions/algsweek5/primcalc/src/main/java/com/github/topefremov/algsweek5/primcalc/PrimitiveCalculator.java
@@ -1,8 +1,10 @@
 package com.github.topefremov.algsweek5.primcalc;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class PrimitiveCalculator {
+
 
     private static String[] operations = { "DIVIDE_BY_3", "DIVIDE_BY_2", "MINUS_1" };
 
@@ -33,44 +35,95 @@ public class PrimitiveCalculator {
     }
 
     static List<Integer> optimal_sequence(int n) {
-        Map<Integer, Integer> variants = new HashMap<>();
-        variants.put(1, 0);
+//        Map<Integer, Integer> variants = new HashMap<>();
+//        variants.put(1, 0);
+//
+//        int minOps;
+//        int m;
+//        for (int i = 2; i <= n; i++) {
+//            minOps = Integer.MAX_VALUE;
+//            for (String operation : operations) {
+//                if (isOperationApplied(operation, i)) {
+//                    m = variants.get(applyOperation(operation, i)) + 1;
+//                    if (m < minOps) {
+//                        minOps = m;
+//                    }
+//                }
+//            }
+//            variants.put(i, minOps);
+//        }
+//
+//
+//        List<Integer> sequence = new ArrayList();
+//        sequence.add(n);
+//        int minSequenceLength = variants.get(n);
+//
+//        while (n > 1) {
+//            for (String operation: operations) {
+//                if (isOperationApplied(operation, n)) {
+//                    int temp = applyOperation(operation, n);
+//                    if (variants.get(temp) < minSequenceLength) {
+//                        n = temp;
+//                        sequence.add(n);
+//                        minSequenceLength--;
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//
+//        return sequence;
 
-        int minOps;
-        int m;
+        List<Integer> result = constructMinSeq(minOps(n));
+        Collections.reverse(result);
+        return result;
+    }
+
+    private static int[] minOps(int n) {
+        int[] minOps = new int[n + 1];
+        minOps[1] = 0;
+
+        int min1, min2, min3;
+
         for (int i = 2; i <= n; i++) {
-            minOps = Integer.MAX_VALUE;
-            for (String operation : operations) {
-                if (isOperationApplied(operation, i)) {
-                    m = variants.get(applyOperation(operation, i)) + 1;
-                    if (m < minOps) {
-                        minOps = m;
-                    }
-                }
+            min1 = minOps[i-1] + 1;
+            min2 = Integer.MAX_VALUE;
+            min3 = Integer.MAX_VALUE;
+
+            if (i % 2 == 0)
+                min2 = minOps[i/2] + 1;
+
+            if (i % 3 == 0) {
+                min3 = minOps[i/3] + 1;
             }
-            variants.put(i, minOps);
+
+            minOps[i] = IntStream.of(min1, min2, min3).min().getAsInt();
+
         }
 
+        return minOps;
+    }
 
-        List<Integer> sequence = new ArrayList();
-        sequence.add(n);
-        int minSequenceLength = variants.get(n);
-
+    private static List<Integer> constructMinSeq(int[] minOps) {
+        int n = minOps.length - 1;
+        List<Integer> seq = new ArrayList<>(Arrays.asList(n));
+        int minSeqLen = minOps[n];
+        int temp;
         while (n > 1) {
-            for (String operation: operations) {
-                if (isOperationApplied(operation, n)) {
-                    int temp = applyOperation(operation, n);
-                    if (variants.get(temp) < minSequenceLength) {
-                        n = temp;
-                        sequence.add(n);
-                        minSequenceLength--;
-                        break;
-                    }
-                }
+            if (n % 3 == 0 && (minSeqLen - minOps[n/3]) == 1) {
+                temp = n / 3;
+            } else  if (n % 2 == 0 && (minSeqLen - minOps[n/2]) == 1) {
+                temp = n / 2;
+            } else {
+                temp = n - 1;
             }
+
+            n = temp;
+            seq.add(n);
+            minSeqLen--;
         }
 
-        return sequence;
+        return seq;
     }
 
     public static void main(String[] args) {
--- solutions/algsweek5/primcalc/src/test/java/com/github/topefremov/algsweek5/primcalc/PrimitiveCalculatorTest.java
+++ solutions/algsweek5/primcalc/src/test/java/com/github/topefremov/algsweek5/primcalc/PrimitiveCalculatorTest.java
@@ -12,12 +12,40 @@ public class PrimitiveCalculatorTest {
 
     @Test
     public void sample1Test() {
-        int n = 5;
-        List<Integer> expected = Arrays.asList(5,4,2,1);
-
+        int n = 1;
+        List<Integer> expected = Arrays.asList(1);
 
         List<Integer> actual = PrimitiveCalculator.optimal_sequence(n);
 
         assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void sample2Test() {
+        int n = 5;
+        List<Integer> expected = Arrays.asList(1,2,4,5);
+
+        List<Integer> actual = PrimitiveCalculator.optimal_sequence(n);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void sample3Test() {
+        int n = 96234;
+        List<Integer> expected = Arrays.asList(1,3,9,10,11,22,66,198,594,1782,5346,16038,16039,32078,96234);
+
+        List<Integer> actual = PrimitiveCalculator.optimal_sequence(n);
+
+        assertThat(actual, is(equalTo(expected)));
+    }
+
+    @Test
+    public void sample4Test() {
+        int n = 98734;
+
+        List<Integer> actual = PrimitiveCalculator.optimal_sequence(n);
+
+        assertEquals(19, actual.size());
     }
 }
